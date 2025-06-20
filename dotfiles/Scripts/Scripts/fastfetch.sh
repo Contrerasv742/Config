34ce4e3b94f9abe1cdb286a15912e8797e648d53
Victor <Contrerasv742@gmail.com>
2025-06-20 11:55:42 -0700

    PC Changes: Waybar larger, Updated Fastfetch script, and working on color matching.

--- dotfiles/Scripts/Scripts/fastfetch.sh
+++ dotfiles/Scripts/Scripts/fastfetch.sh
@@ -7,18 +7,17 @@ POKEMON_DIR="$HOME/.config/fastfetch/imgs"
 IMAGE_FILES=($POKEMON_DIR/*)
 
 # Check if there are any image files
-# if [ ${#IMAGE_FILES[@]} -eq 0 ]; then
-#     echo "No image files found in $POKEMON_DIR"
-#     exit 1
-# fi
+if [ ${#IMAGE_FILES[@]} -eq 0 ]; then
+    echo "No image files found in $POKEMON_DIR"
+    exit 1
+fi
 
 # Choose a random image from the array
 RANDOM_POKEMON=${IMAGE_FILES[$RANDOM % ${#IMAGE_FILES[@]}]}
 
 # Print which Pokémon was selected (optional)
-# echo "Selected Pokémon: $(basename "$RANDOM_POKEMON")"
+echo "Selected Pokémon: $(basename "$RANDOM_POKEMON")"
 
 # Run fastfetch with the random Pokémon
 CONFIG="$HOME/.config/fastfetch/arch.jsonc"
-
-fastfetch --config "$CONFIG" --logo "$RANDOM_POKEMON" --logo-width 25 --logo-height 15
+fastfetch --config "$CONFIG" --kitty-icat "$RANDOM_POKEMON" --logo-width 25 --logo-height 25
